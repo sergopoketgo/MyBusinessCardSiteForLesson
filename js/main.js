6a2b18f36e96771034a84f0b6719ec6022c60b5f
sergopoketgo <sergiy.gogaev@gmail.com>
2024-03-24 03:40:19 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    let $logoContainer = $("#logo-container");
+
+    $logoContainer.click(function () {
+        window.open("https://github.com/sergopoketgo");
+    });
+});
