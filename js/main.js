569aeb3743a63db084ccca5609703c1b88cc837f
sergopoketgo <sergiy.gogaev@gmail.com>
2024-03-24 03:58:59 +0200

    add scroll on click

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 $(document).ready(function () {
-    let $logoContainer = $("#logo-container");
+    let $logoContainer = $("#logo-container"),
+        btnDown = $("#btn-down");
 
     $logoContainer.click(function () {
         window.open("https://github.com/sergopoketgo");
+    });
+
+    btnDown.click(function () {
+        $("html").animate({scrollTop: $(window).height()}, 200);
     });
 });
